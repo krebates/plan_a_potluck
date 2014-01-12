c6eb5514b47924171695a0587d0053b4868f27ec
Krystle Bates <krebates@gmail.com>
2014-01-11 22:26:13 -0500

    updated the token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-PotluckApp2::Application.config.secret_key_base = 'd08490d57eeebc5dc87b82d1949f080f8970ca5a644c0085995c09d28c8fe8ce501907f49b67e16d30e802961e04be63856af0ed8b60b8515cb8dc2ad743513e'
+PotluckApp2::Application.config.secret_key_base =
